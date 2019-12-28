8b5197514ad181d51f7e787b5315b962977b65cb
nignanthomas <nignanthomas@gmail.com>
2019-12-28 15:37:48 +0200

    chore(setup-project): Setup React Project
    - create react project from scratch with Webpack Babel
    
    - setup jest testing
    
    - setup routing
    
    - setup codeclimate and travis setup sass
    
    - setup eslint
    
    [Finishes #170461909]

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    publicPath: '/',
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss|css)?$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
